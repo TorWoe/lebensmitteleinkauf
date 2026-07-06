b5a11510d9651f0918c4b6603ac82444e424e9e4
TorWoe <torsten.woebken@googlemail.com>
2026-07-06 20:34:21 +0200

    Stack OneDrive action buttons

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const { foods, meals, sources } = window.APP_DATA;
-  const appVersion = "manual-login-renew-20260706-1";
+  const appVersion = "sync-buttons-stacked-20260706-1";
   const appVersionFile = "app-version.json";
   const appRefreshParam = "appRefresh";
   const appRefreshSessionKey = "lebensmitteleinkauf:app-refresh-version:v1";
